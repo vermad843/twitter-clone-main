6ca7ab27af4a571e5872ae17c607f526a56e99f9
vermad843 <vishnuverma7524@gmail.com>
2021-04-23 22:06:20 +0530

    sidebar content

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,13 @@ function Sidebar() {
     return (
         <div className = "sidebar">
              <h1>I am a Sidebar Component</h1>
+              {/* Twitter Icon */}
+              {/* SidebarOption */}
+              {/* SidebarOption */}
+              {/* SidebarOption */}
+              {/* SidebarOption */}
+              {/* SidebarOption */}
+              {/* SidebarOption */}
         </div>
     )
 }
