6762b591c37a0603ea4e0ed88045bb1923ab02fc
vermad843 <vishnuverma7524@gmail.com>
2021-04-23 22:02:24 +0530

    I am a Sidebar Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
-import React from 'react'
+import React from 'react';
+import Sidebar from './Sidebar';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-       <h1>Hello World </h1>
+       <Sidebar/>
        {/* <Sidebar/> */}
        {/* <Feed/> */}
        {/* <Widget/> */}
---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+    return (
+        <div className = "sidebar">
+             <h1>I am a Sidebar Component</h1>
+        </div>
+    )
+}
+
+export default Sidebar;
