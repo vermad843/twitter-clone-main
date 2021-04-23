e06c98c565df1783217ad0b330b337a20a123240
vermad843 <vishnuverma7524@gmail.com>
2021-04-23 21:56:04 +0530

    different component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-       <h1>Hello World</h1>
+       <h1>Hello World </h1>
+       {/* <Sidebar/> */}
+       {/* <Feed/> */}
+       {/* <Widget/> */}
     </div>
   );
 }
