8fab588f6091df85c884a476964f109842964537
vermad843 <vishnuverma7524@gmail.com>
2021-04-23 23:06:39 +0530

    more icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,11 @@ import SidebarOption from './SidebarOption';
 import HomeIcon from '@material-ui/icons/Home'
 import SearchIcon from '@material-ui/icons/Search';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 function Sidebar() {
     return (
@@ -14,8 +19,11 @@ function Sidebar() {
               <SidebarOption Icon = {HomeIcon} text = "Home" />
               <SidebarOption Icon = {SearchIcon} text = "Explore"/>
               <SidebarOption Icon = {NotificationsNoneIcon} text = "Notifications"/>
-              {/* <SidebarOption/> */}
-              {/* <SidebarOption/> */}
+              <SidebarOption Icon = {MailOutlineIcon} text = "Messages"/>
+              <SidebarOption Icon = {BookmarkBorderIcon} text = "Bookmarks"/>
+              <SidebarOption Icon = {ListAltIcon} text = "Lists"/>
+              <SidebarOption Icon = {PersonOutlineIcon} text = "Profile"/>
+              <SidebarOption Icon = {MoreHorizIcon} text = "More"/>            
         </div>
     )
 }
