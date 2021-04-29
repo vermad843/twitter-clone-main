69a574c5fb65d0959520c9b66750c76b4e17a3be
vermad843 <vishnuverma7524@gmail.com>
2021-04-29 06:49:33 -0400

    Post Component

--- src/Feed.js
+++ src/Feed.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TweetBox from './TweetBox';
+import Post from './Post';
 import './Feed.css';
 
 function Feed() {
@@ -9,6 +10,7 @@ function Feed() {
               <h2>Home</h2>        
             </div>
             <TweetBox/>
+            <Post/>
         </div>
     )
 }
---
+++ src/Post.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Post.css';
+
+function Post() {
+    return (
+        <div className = "post">
+            <h1>I am a Post Component</h1>
+        </div>
+    )
+}
+
+export default Post;
